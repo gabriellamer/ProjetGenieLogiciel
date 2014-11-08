ccbba98237ea85736936092efba07a9ad0c6b22d
SimCitee <simon.gdupre@gmail.com>
2014-11-08 12:39:58 -0500

    Copy source version scrap

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.awt.EventQueue;
+
+import ca.uqtr.gl.controllers.ControlleurClients;
+import ca.uqtr.gl.ui.EcranGestionClient;
+
+public class Main {	
+	public static void main(String[] args) {	
+		final ControlleurClients ctlClients = new ControlleurClients();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EcranGestionClient window = new EcranGestionClient(ctlClients);
+					window.frmAjoutmajsuppressionClient.setVisible(true);					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
